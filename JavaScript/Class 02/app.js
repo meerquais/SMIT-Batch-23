15967b33c00134a46b6b10327eaa20d720644d7d
Meer Quais <meerquais017@gmail.com>
2026-09-14 17:50:48 +0500

    Variables and Math Expressions done

---
+++ JavaScript/Class 02/app.js
@@ -0,0 +1,169 @@
+console.log("JavaScript Connected!");
+
+
+// var user = "Minhaj";
+
+
+// // myName variable declare huwa hai.
+
+// var myName;
+
+
+// // myName variable ko initialize kar diya
+// myName = "Meer";
+
+
+
+// // reinitialize variable
+
+// user = "Ayan"
+
+// console.log(user);
+
+
+// illegal variable names!
+
+
+// var 2users = "two";
+
+// var if = "if";
+// var var;
+// var for = "for me ";
+
+// var %test =
+// var &test =
+
+
+// legal variable names
+
+// var user2 = "Two";
+
+// var $test = "test";
+
+// Camel Case // Snake Case // Pascal Case
+
+
+// 1. Camel Case
+
+
+// var saylani = "Saylani";
+
+// var saylaniMass = "Saylani Mass";
+
+// var saylaniMassIT = "Saylani Mass IT";
+
+// var saylaniMassItTraining = "Saylani Mass IT Training"
+
+// 2. Snake Case
+
+
+// var saylani = "Saylani";
+
+// var saylani_mass = "Saylani Mass";
+
+
+// 3.Pascal Case
+
+// var Saylani = "Saylani";
+
+// var SaylaniMass = "Saylani Mass";
+
+
+// primatives data types // non-primatives data types 
+
+
+// primative data types
+
+// String
+// Number
+// Boolean
+// Undefiend
+// Null 
+
+// Non-Primatives data types
+
+// Array
+// Functions
+// Objects
+
+
+// String
+
+
+var user = "This is a String.";
+
+
+// Number 
+
+
+var num = 100;
+
+
+// Boolean
+
+var isLoggedIn = false;
+var isHere = true;
+
+// Undefined 
+
+var isComing = undefined;
+
+var willBeHere;
+
+// console.log(willBeHere);
+
+
+// null
+
+var userData = null;
+
+
+
+
+// var num1 = 20;
+
+// var num2 = 40;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 / num2);
+// console.log(num1 * num2);
+// console.log(num1 % num2);
+
+
+// var num = 10;
+
+
+// console.log(++num);
+
+// console.log(num++);
+// console.log(num);
+
+
+
+// var a = 5;//6//7//6//5//6/7/8
+
+
+// //           5   +  7  -  6  - 6   + 6   + 6 + 6   + 7 + 7 + 8
+// var result = a++ + ++a - --a - a-- + ++a + a + a++ + a + a + ++a;
+
+// console.log(result);
+
+
+
+
+// var a = 10;// 9/8/7/8/9/10/11/12/13/12/13 
+
+
+//           9  -  8   - 8  +  7  + 9   - 10 +  10  - 12  + 12  + 13 + 13 + 12 - 12 - 12 + 13 + 13
+
+// var result = --a - --a - a-- + a++ + ++a - ++a + a++ - ++a + a++ + a + a + --a - a - a + ++a + a;
+
+// console.log(result);
+
+
+
+// console.log(29 * (20 / 20));
+
+console.log((20 + 20) * 40);
+
