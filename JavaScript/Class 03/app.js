ac4b7aef3a2b39f44819978dc016893617746828
Meer Quais <meerquais017@gmail.com>
2026-09-16 17:59:05 +0500

    If Statements

---
+++ JavaScript/Class 03/app.js
@@ -0,0 +1,97 @@
+console.log("JavaScript Connected!");
+
+
+// var user = "Minhaj";
+// var msg = "Welcome to our Website";
+
+
+// alert(msg + " " + user)
+
+
+// console.log(20 + "20");
+// console.log(20 - "20");
+
+
+
+// var user = "Ali";
+// var msg = "Thanks for your input!";
+
+// console.log(msg + " " + user);
+
+
+// var user = prompt("Type your name");
+// var msg = prompt("Type the message");
+
+// console.log(msg + " " + user);
+
+
+
+// if(condition){
+//     // block of code
+// }
+
+// var userInput = prompt("Type your city name!");
+// var cityName = "Karachi";
+
+
+// if (userInput === cityName) {
+//     alert("Congrats!");
+// }
+
+// if(condition){
+//     block of code
+// }else{
+//     // else block of code
+// }
+
+
+// var userInput = prompt("Type your city name");
+// var cityName = "Karachi";
+
+
+// if (userInput === cityName) {
+//     alert("Your city Matches!");
+// } else {
+//     alert("Does not Match our data!");
+// }
+
+
+
+// assigning operator ===>   " =  "
+// Equality Operator ===>  " == "    ===> 1 == "1"
+// Strictly Equality Operator ===>  " === " 1 === "1"
+
+// greater than ===> " > "   value bari ho
+// less than ====> " < "    value choti ho
+// greater than equal to ===> " >= " value barabar ya phir bari ho
+// less than equals to ====> " <= " value barabar ya phir choti ho
+
+// Not equals to ====> " != "
+// strictly not equals to  ===> " !== " 
+
+
+
+
+console.log(1 == "1");
+console.log(2 == "1");
+
+console.log(1 === "1");
+console.log(1 === 1);
+
+
+console.log(5 > 10);
+console.log(15 > 10);
+console.log(15 >= 10);
+console.log(10 >= 10);
+
+
+console.log(2 != "1");
+
+console.log(2 !== 1);
+
+
+
+
+
+
+
