c67efbdbeb766d6865c89630be8f07cbaa88020b
Meer Quais <meerquais017@gmail.com>
2026-09-18 17:55:47 +0500

    If statements compeleted

---
+++ JavaScript/Class 04/app.js
@@ -0,0 +1,191 @@
+console.log("JavaScript Connected!");
+
+
+// var age = 19;
+
+// if (age >= 18) {
+//     console.log("Adult!");
+// }
+
+
+// var temperature = 35;
+
+// if (temperature > 30) {
+//     console.log("Bohot garmi hai");
+// }
+
+// var isOnline = false;
+
+
+// if (isOnline) {
+//     console.log("User online hai");
+// }
+
+
+// var marks = 75;
+
+
+// if (marks >= 50) {
+//     console.log("Pass");
+// } else {
+//     console.log("Fail");
+// }
+
+
+// var password = "admin";
+
+
+// if (password === "admin") {
+//     console.log("Access Granted!");
+// } else {
+//     console.log("Access Denied!");
+// }
+
+
+// var score = 75;
+
+
+// if (score >= 90) {
+//     console.log("Grade A");
+// } else if (score >= 80) {
+//     console.log("Grade B");
+// } else if (score >= 70) {
+//     console.log("Grade C");
+// } else if (score >= 60) {
+//     console.log("Grade D");
+// } else {
+//     console.log("Failed!");
+// }
+
+
+// var time = 14;
+
+// if (time < 12) {
+//     console.log("Good Morning!");
+// } else if (time < 18) {
+//     console.log("Good Afternoon");
+// } else {
+//     console.log("Good night!");
+// }
+
+
+// var role = "users";
+
+
+// if (role === "admin") {
+//     console.log("Full Access");
+// } else if (role === "editor") {
+//     console.log("Editing Access");
+// } else if (role === "user") {
+//     console.log("Limited Access");
+// } else {
+//     console.log("No Access");
+// }
+
+
+// AND Operator  ====>  " && "
+// OR Operator   ====>  " || "
+
+
+// var age = 20;
+// var hasCNIC = true;
+// //    true           true     ===> True
+// if (age >= 18 && hasCNIC === true) {
+//     console.log("Aap Vote de sakhte ho.");
+// }
+
+
+
+// var isAdmin = false;
+// var isModerator = true;
+
+
+// if (isAdmin || isModerator) {
+//     console.log("Dashboard access!");
+// }
+
+
+
+// var age = 25;
+// var city = "Karachi";
+// var hasTicket = true;
+
+
+// if ((age >= 18 && city === "Karachi") || hasTicket) {
+//     console.log("Entry Allowed!");
+// }
+
+// var age = 22;
+// var hasCNIC = true;
+
+
+
+// if (age >= 18) {
+
+//     if (hasCNIC === true) {
+//         console.log("Voting Allowed!");
+//     }
+// }
+
+
+// var num = +prompt("Type any number");
+
+
+// if (num % 2 === 0) {
+//     console.log("Even Number");
+// } else {
+//     console.log("Odd Number");
+// }
+
+
+// var str = "This is String";
+// var num = 12345;
+// var boolean = true;
+// var undef = undefined;
+// var nothing = null;
+
+// console.log(typeof str, str);
+// console.log(typeof num, num);
+// console.log(typeof boolean, boolean);
+// console.log(typeof undef, undef);
+// console.log(typeof nothing, nothing);
+
+
+
+// var num1 = +prompt("Type 1st number");
+// var opt = prompt("Type your operator ex: +/-/*/ / %");
+// var num2 = +prompt("Type 2nd number");
+
+
+// if (opt === "*") {
+//     console.log(num1 * num2);
+// } else if (opt === "/") {
+//     console.log(num1 / num2);
+// } else if (opt === "+") {
+//     console.log(num1 + num2);
+// } else if (opt === "-") {
+//     console.log(num1 - num2);
+// } else if (opt === "%") {
+//     console.log(num1 % num2);
+// } else {
+//     console.log("Operator does not match!");
+// }
+
+
+
+
+var fruit1 = "Apple";
+var fruit2 = "Orange";
+var fruit3 = "Banana";
+var fruit4 = "Strawberry";
+
+
+
+var fruits = ["Apple", "Orange", "Mango", "Banana", "Strawberry"];
+
+
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
+console.log(fruit4);
+console.log(fruits);
