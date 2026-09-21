da59a85ce4298fc593507e51f215b88cd3ff9a05
Meer Quais <meerquais017@gmail.com>
2026-09-21 17:57:16 +0500

    For Loop Started

---
+++ JavaScript/Class 05/app.js
@@ -0,0 +1,105 @@
+console.log("JavaScript Connected!");
+
+
+// var fruit1 = "Apple";
+// var fruit2 = "Mango";
+// var fruit3 = "Banana";
+// var fruit4 = "Strawberry";
+// var fruit5 = "Peach";
+// var fruit6 = "Leechy";
+
+// var fruits = ["Apple", "Mango", "Banana", "Strawberry", "Watermelon", "Peach"];
+
+// console.log(fruits);
+// console.log(fruits[3]);
+
+
+
+// var arr = ["Meer", 26, true, undefined, null, ["Apple", "Mango", "Orange"]];
+
+// console.log(arr);
+
+// var cities = ["Karachi", "Lahore", "Islamabad", "Peshwar", "Quetta", "Faisalabad"];
+
+// console.log(cities);
+
+
+// var fruits = ["Apple", "Mango", "Banana", "Strawberry", "Watermelon", "Peach"];
+
+// console.log(fruits);
+
+// var del = fruits.pop()
+
+// console.log(fruits);
+// console.log(del);
+
+// fruits.push("Orange", "Peach")
+// console.log(fruits);
+
+// fruits.shift()
+// console.log(fruits);
+// fruits.unshift("Dragonfruit");
+// console.log(fruits);
+
+
+
+// splice(start, deleteCount , ...item)
+
+
+// var fruits = ["Apple", "Mango", "Banana", "Strawberry", "Watermelon", "Peach"];
+
+// console.log(fruits);
+// fruits.splice(3, 3, "Dragonfruit", "Orange");
+// console.log(fruits);
+// fruits.splice(-1, 0, "Dragonfruit");
+// console.log(fruits);
+
+// var copyfruits = fruits.slice(1, 3);
+// console.log(copyfruits);
+
+
+// slice ====> copy a portion of array
+// splice ===> modification in array
+
+// arr.slice(start , end)
+// arr.splice(start,deleteCount, ...item)
+
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+// console.log("Welcome");
+
+
+// for loop syntax
+
+
+// for(initialization;condition;incre/decre(update)){
+// block of code
+// }
+
+
+// for (var i = 1; i <= 20; i++) {
+//     console.log("Welcome");
+// }
+
+
+
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
