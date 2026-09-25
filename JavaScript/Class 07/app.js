77a6530be76b15ac1a9fe00e25a18cc15a3a30cb
Meer Quais <meerquais017@gmail.com>
2026-09-25 17:59:20 +0500

    For Loop Completed

---
+++ JavaScript/Class 07/app.js
@@ -0,0 +1,186 @@
+console.log("JavaScript Connected!");
+
+
+
+
+
+
+// for (var i = 0; i < 5; i++) {
+
+
+//     for (var j = 0; j < 4; j++) {
+//         console.log(i, j);
+//     }
+
+
+
+// }
+
+
+// var firstNames = [
+//     "BlueRay",
+//     "Upchuck",
+//     "Lojack",
+//     "Gizmo",
+//     "Do-Rag"
+// ]
+
+// var lastNames = [
+//     "Zzz",
+//     "Burp",
+//     "Dogbone",
+//     "Droop"
+// ]
+
+// var fullNames = [];
+
+
+// for (var i = 0; i < firstNames.length; i++) {
+
+
+//     for (var j = 0; j < lastNames.length; j++) {
+
+
+//         fullNames.push(firstNames[i] + " " + lastNames[j]);
+
+//     }
+
+
+// }
+
+// console.log(fullNames);
+
+
+
+
+// for (var i = 1; i < 11; i++) {
+
+//     console.log("Outer Loop" + " " + i);
+
+
+//     for (var j = 1; j < 11; j++) {
+//         console.log("Inner Loop");
+//     }
+
+
+
+// }
+
+
+// alert("This is an Alert!");
+
+// var msg = "This is an Alert!";
+
+
+// alert(msg);
+
+
+// var user = "Minhaj";
+
+// console.log(user);
+
+
+// var userName;
+
+// userName = "Ayan";
+
+// console.log(userName);
+
+// userName = "Hafeez";
+
+// console.log(userName);
+
+
+// String ====> "Text"
+
+// Number ====>  12345
+
+// Boolean ===> true or false
+
+// undefiend => undefiend
+
+// Null   ====> Null
+
+
+// var user = "Ayan";
+// var msg = "Welcome to our Webpage";
+
+// console.log(msg + " " + user);
+
+// var num = prompt("Type your fav value!")
+
+// console.log(typeof num, num);
+
+
+// var age = 15;
+
+
+// if (age === 18) {
+//     console.log("Allowed!");
+// } else {
+//     console.log("Not Allowed!");
+// }
+
+
+// var marks = 20;
+
+
+// if (marks >= 90) {
+//     console.log("Grade A");
+// } else if (marks >= 80) {
+//     console.log("Grade B");
+// } else if (marks >= 70) {
+//     console.log("Grade C");
+// } else if (marks >= 60) {
+//     console.log("Grade D");
+// } else {
+//     console.log("You Failed!");
+// }
+
+
+// OR Gate // AND Gate ====> OR ====> ||  =====> AND GATE ====> &&
+
+
+
+
+//      true
+// if (true || false) {
+//     console.log("True");
+// }
+
+
+// if (true && true) {
+//     console.log("True");
+// } else {
+//     console.log("False");
+// }
+
+
+// var arr = [] ; 
+
+// var fruits = ["Mango", "Apple", "Orange", "Banana"];
+
+// console.log(fruits[2]);
+
+
+// fruits.pop();
+
+// fruits.push("Watermelon");
+
+// console.log(fruits);
+
+// var copyArr = fruits.slice(1, 3);
+
+// copyArr.push("Watermelon");
+
+// console.log(copyArr);
+// console.log(fruits);
+
+
+var fruits = ["Mango", "Apple", "Orange", "Banana"];
+
+console.log(fruits);
+fruits.splice(2, 0, "Watermelon", "Strawberry", "Dragonfruit");
+console.log(fruits);
+
+
